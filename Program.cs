a23ad2aeaa6ad0a86cb588d855c42faf2929b2c6
Rick Slor <rickslor.rs@gmail.com>
2022-12-05 14:54:47 +0100

    Failing at understanding the 2D Camera
    Trying stuff with the camera

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Numerics;
 using Raylib_cs;
+
 namespace Nonogram // Note: actual namespace depends on the project name.
 
 {
@@ -10,9 +12,16 @@ namespace Nonogram // Note: actual namespace depends on the project name.
             Puzzel p = new Puzzel();
             const int screenWidth = 800;
             const int screenHeight = 450;
+            const int x = screenWidth;
+            const int y = screenHeight;
 
-            Camera2D camera = new Camera2D();
-            camera.zoom = 1.0f;
+            Camera2D camera = new Camera2D()
+            {
+                target = new Vector2(x + ((screenWidth / 2) + 800), y + ((screenHeight / 2) + 450)),
+                offset = new Vector2(screenWidth, screenHeight),
+                rotation = 0.0f,
+                zoom = 1.0f,
+            };
 
             Raylib.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
@@ -33,7 +42,7 @@ namespace Nonogram // Note: actual namespace depends on the project name.
                 Raylib.ClearBackground(Color.WHITE);
 
                  Raylib.BeginMode2D(camera);
-                Raylib.DrawGrid(100, 50);
+                Raylib.DrawGrid(10, 1);
                 Raylib.EndMode2D();
 
                 Raylib.DrawText("Congrats! You created your first window!", 190, 200, 20, Color.LIGHTGRAY);
